f4fa97fcf5301c0ded88f7a81a753afe623fe87d
Ever400 <evrodriguez@s.sfusd.edu>
2017-11-27 18:06:04 -0800

    ukhb

--- main.js
+++ main.js
@@ -1,104 +1,49 @@
-var mainState = {
-    preload: function(){
-        game.load.image('player','assets/player.png');
-        game.load.image('wall','assets/wall.png');
-        game.load.image('coin','assets/coin.png');
-        game.load.image('enemy','assets/enemy.png');
-    },
+
+
+preload function:
+preload: function() { 
+// Here we preload the assets
+
+//name image on the left, give path to image on the right 
+game.load.image('player', 'Official/Banana.png');
+game.load.image('wall', 'Official/BCFS6230-Bavarian-Castle-Westwood.jpg');
+},
+  
     
-    create: function(){
-        //change the game's background color 
-        game.state.backgroundColor = '#3598db';
-        //start physics system for movements and collisions
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-        //add hte physics engine to all the game objects
-        game.world.enableBody = true;
-        
-        this.cursor = game.input.keyboard.createCursorKeys();
-        
-        //creates player in the center of the game 
-        this.player = game.add.sprite(70,100, 'player');
-        
-        this.player.body.gravity.y = 600;
-        
-        this.walls = game.add.group();
-        this.coins = game.add.group();
-        this.enemies = game.add.group();
-        
-        
-//Game settings/player setup
+    Game settings/player set up in Create():
+//change the game's background color
+game.stage.backgroundColor = '#3598db';
 
-// Create 3 groups that will contain our objects
-this.walls = game.add.group();
-this.coins = game.add.group();
-this.enemies = game.add.group();
+// Start the Arcade physics system (for movements and collisions)
+game.physics.startSystem(Phaser.Physics.ARCADE);
 
-// Design the level. x = wall, o = coin, ! = lava.
-var level = [
-'xxxxxxxxxxxxxxxxxxxxxxxx',
-'!            !         x',
-'!                   o  x',
-'!            o         x',
-'!                      x',
-'!        o !           x',
-'xxxxxxxxxxxxxxxxxx!!!!!x',
-];
+// Add the physics engine to all game objects
+game.world.enableBody = true;
 
-// Create the level by going through the array
-for (var i = 0; i < level.length; i++) {
-    for (var j = 0; j < level[i].length; j++) {
+this.cursor = game.input.keyboard.createCursorKeys();
 
-// Create a wall and add it to the 'walls' group
-if (level[i][j] == 'x') {
-var wall = game.add.sprite(30+20*j, 30+20*i, 'wall');
-this.walls.add(wall);
-wall.body.immovable = true; 
+// Create the player in the middle of the game
+this.player = game.add.sprite(70, 100, 'player');
+
+// Add gravity to make it fall
+this.player.body.gravity.y = 600;
+
+
+
+Keyboard controls in Update():
+
+// Move the player when an arrow key is pressed
+if (this.cursor.left.isDown){ 
+this.player.body.velocity.x = -200;
+} else if (this.cursor.right.isDown) {
+this.player.body.velocity.x = 200;
+} else {
+this.player.body.velocity.x = 0;
 }
-
-// Create a coin and add it to the 'coins' group
-else if (level[i][j] == 'o') {
-var coin = game.add.sprite(30+20*j, 30+20*i, 'coin');
-this.coins.add(coin);
-}
-
-// Create a enemy and add it to the 'enemies' group
- else if (level[i][j] == '!') {
-var enemy = game.add.sprite(30+20*j, 30+20*i, 'enemy');
-this.enemies.add(enemy);
-      }
-   }
-}
-    },
-        
-    update: function(){
-        game.physics.arcade.collide(this.player,this.walls);
-        
-        game.physics.arcade.overlap(this.player,this.coins, this.takeCoin, null, this);
-        
-        game.physics.arcade.overlap(this.player, this.enemies, this.restart, null, this);
-        
-        if(this.cursor.left.isDown){
-            this.player.body.velocity.x = -200;
-        }else if(this.cursor.right.isDown){
-            this.player.body.velocity.x = 200;
-        }else{
-            this.player.body.velocity.x = 0;
-        }
-        
-        if(this.cursor.up.isDown && this.player.body.touching.down){
-            this.player.body.velocity.y = -250;
-        }
-    },
-    
-    takeCoin: function(player, coin){
-        coin.kill();
-    },
-    
-    restart: function(){
-        game.state.start('main');
-    }
-};
+// Make the player jump if he is touching the ground
+if (this.cursor.up.isDown && this.player.body.touching.down) 
+this.player.body.velocity.y = -250;
 
 var game = new Phaser.Game(500,200);
-game.state.add('main', mainState);
+game.state.add('main', mainstate);
 game.state.start('main');
